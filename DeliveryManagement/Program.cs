bc856b071d637eb835acf72e8b06220eb42a2f5d
bottomtext228 <yarik.popov2015@yandex.ru>
2024-04-10 17:14:46 +0300

    Added catalog.

--- DeliveryManagement/Program.cs
+++ DeliveryManagement/Program.cs
@@ -33,23 +33,23 @@ builder.Services.AddIdentity<User, IdentityRole>(options =>
 
 var app = builder.Build();
 
-//using (var scope = app.Services.CreateScope())
-//{
-//    var services = scope.ServiceProvider;
-//    try
-//    {
-//        var userManager = services.GetRequiredService<UserManager<User>>();
-//        var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-//        var dbContext = services.GetRequiredService<ApplicationDbContext>();
-//        await RoleInitializer.InitializeAsync(userManager, rolesManager, dbContext);
-//    }
-//    catch (Exception ex)
-//    {
-//        var logger = services.GetRequiredService<ILogger<Program>>();
-//        logger.LogError(ex, "An error occurred while seeding the database.");
-//    }
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var userManager = services.GetRequiredService<UserManager<User>>();
+        var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        var dbContext = services.GetRequiredService<ApplicationDbContext>();
+        await RoleInitializer.InitializeAsync(userManager, rolesManager, dbContext);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding the database.");
+    }
 
-//}
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
