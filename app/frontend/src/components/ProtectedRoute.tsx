39e6c2395625c8a6ad41c5e4e100a1bb5bef6369
bottomtext228 <yarik.popov2015@yandex.ru>
2025-03-11 20:32:00 +0300

    Change something

--- app/frontend/src/components/ProtectedRoute.tsx
+++ app/frontend/src/components/ProtectedRoute.tsx
@@ -1,6 +1,5 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth"
-import { useEffect } from "react";
 
 interface ProtectedRouteProps {
     children: JSX.Element
@@ -8,19 +7,20 @@ interface ProtectedRouteProps {
 
 export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
     const isAuth = useAuth();
-    const navigate = useNavigate();
-    useEffect(() => {
-        if (!isAuth) { navigate('/auth/login'); }
-    }, [])
-
-    if (!isAuth) return <></>;
-
-    return children;
-    /* return <>
+    const location = useLocation();
+    /*    const navigate = useNavigate();
+         useEffect(() => {
+            if (!isAuth) { navigate('/auth/login'); }
+        }, [])
+    
+        if (!isAuth) return <></>;
+    
+        return children; */
+    return <>
         {isAuth ? children :
             <div>
                 <h1>To view this page you must be logged in.</h1>
-                <Link to='/auth/login' replace state={{ returnUrl: location, action: 'login' }} >Login</Link>
+                <Link to='/auth/login' replace state={{ returnUrl: location }} >Login</Link>
             </div>}
-    </> */
+    </>
 }
