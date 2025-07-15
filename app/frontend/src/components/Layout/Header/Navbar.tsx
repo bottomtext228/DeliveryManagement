d5a1b3998dd5bf9e8693cd19386026d993631f87
bottomtext228 <yarik.popov2015@yandex.ru>
2025-07-16 00:24:49 +0300

    Swapped mobile Navbar and Dropdown positions

--- app/frontend/src/components/Layout/Header/Navbar.tsx
+++ app/frontend/src/components/Layout/Header/Navbar.tsx
@@ -24,13 +24,13 @@ export default function Navbar() {
     }
     return (
         <>
-            <button className='mr-auto md:hidden' onClick={() => setIsDropdownOpen(true)}>
-                <img className='w-12 h-12 opacity-80' src='/person.svg'></img>
-            </button>
-            <button className="ml-auto md:hidden" onClick={() => setIsNavbarOpen(true)}>
+            <button className="mr-auto md:hidden" onClick={() => setIsNavbarOpen(true)}>
                 <img className='w-12 h-12 opacity-80' src='/bars.svg'></img>
             </button>
-            <div className={`md:hidden bg-white p-0.5 h-full fixed top-0 w-full max-w-[15em] ${isDropdownOpen ? 'left-0' : '-left-full'} z-50 transition-all duration-300 ease-in-out`}>
+            <button className='ml-auto md:hidden' onClick={() => setIsDropdownOpen(true)}>
+                <img className='w-12 h-12 opacity-80' src='/person.svg'></img>
+            </button>
+            <div className={`md:hidden bg-white p-0.5 h-full fixed top-0 w-full max-w-[15em] ${isDropdownOpen ? 'right-0' : '-right-full'} z-50 transition-all duration-300 ease-in-out`}>
                 <div className='relative flex h-8 text-center'>
                     <button className="absolute left-0 top-[50%] -translate-y-[50%] flex transform items-center justify-center w-8 h-8 opacity-75 md:hidden z-10" onClick={() => setIsDropdownOpen(false)}>
                         <img src='/cross.svg' className="w-4 h-4"></img>
@@ -88,7 +88,7 @@ export default function Navbar() {
 
             </div >
 
-            <nav id='nav' className={`md:flex md:justify-around md:static  md:items-center md:bg-transparent bg-white p-0.5 md:w-8xl md:max-w-full md:h-24 h-full fixed top-0 w-full max-w-[15em] ${isNavbarOpen ? 'right-0' : '-right-full'} z-50 transition-all duration-300 ease-in-out`}>
+            <nav id='nav' className={`md:flex md:justify-around md:static  md:items-center md:bg-transparent bg-white p-0.5 md:w-8xl md:max-w-full md:h-24 h-full fixed top-0 w-full max-w-[15em] ${isNavbarOpen ? 'left-0' : '-left-full'} z-50 transition-all duration-300 ease-in-out`}>
                 <div className="md:flex md:justify-center md:items-center">
                     <Link className="hidden md:block" to='/'><img src="/logo.png" className="w-64"></img></Link>
                     <div className='relative flex h-8 text-center'>
